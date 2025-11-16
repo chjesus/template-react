0bdd22176fe729cd0547f35f710cd6f353186cef
Jesus Chacon <chjesussao@gmail.com>
2025-11-16 01:12:42 -0400

    configuracion de rutas de importaciones

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './app'
+import App from '@app/App'
 
-createRoot(document.getElementById('root')!).render(
+const CONTAINER = document.getElementById('root')!
+const ROOT = createRoot(CONTAINER!)
+
+// ===============|| MAIN - REACT DOM RENDER  ||=============== //
+
+ROOT.render(
 	<StrictMode>
 		<App />
 	</StrictMode>
--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { resolve } from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  server: {
-    open: true,
-    strictPort: true,
-    port: 3000,
-  },
-  plugins: [react()],
+	server: { open: true, strictPort: true, port: 3000 },
+	define: { global: 'window' },
+	resolve: {
+		alias: [
+			{ find: '@app', replacement: resolve(__dirname, '/src/app') },
+			{ find: '@pages', replacement: resolve(__dirname, '/src/pages') },
+			{ find: '@shared', replacement: resolve(__dirname, '/src/shared') },
+			{ find: '@widgets', replacement: resolve(__dirname, '/src/widgets') },
+			{ find: '@entities', replacement: resolve(__dirname, '/src/entities') },
+		],
+	},
+	plugins: [react()],
 })
